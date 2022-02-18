1a562cb1b05b010d6a25bfc62249779e98fdae98
lokeshwaran26 <94343329+lokeshwaran26@users.noreply.github.com>
2022-02-18 16:01:59 +0530

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,41 @@
+import datetime
+from Google import Create_Service
+from googleapiclient.http import MediaFileUpload
+
+def youtube(File,File2,File3):
+    CLIENT_SECRET_FILE = 'client_secret_991340592645-imdkpppqot8ftjis7a9825ng043gfare.apps.googleusercontent.com.json'
+    API_NAME = 'youtube'
+    API_VERSION = 'v3'
+    SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
+
+    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+
+    upload_date_time = datetime.datetime(2020, 12, 25, 12, 30, 0).isoformat() + '.000Z'
+
+    request_body = {
+        'snippet': {
+            'categoryI': 19,
+            'title': File3,
+            #'description': 'Love Tunes',
+            #'tags': ['Love', 'True', 'Bgm', 'Tune']
+        },
+        'status': {
+            'privacyStatus': 'private',
+            'publishAt': upload_date_time,
+            #'selfDeclaredMadeForKids': False,
+        },
+        #notifySubscribers': False
+    }
+
+    mediaFile = MediaFileUpload(File)
+
+    response_upload = service.videos().insert(
+        part='snippet,status',
+        body=request_body,
+        media_body=mediaFile
+    ).execute()
+
+    service.thumbnails().set(
+        videoId=response_upload.get('id'),
+        media_body=MediaFileUpload(File2)
+    ).execute()
